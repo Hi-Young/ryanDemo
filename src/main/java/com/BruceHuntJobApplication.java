241b9177ac24cd9eeff506c9f166f2d3ad7787cc
heyyon <emailformhy@163.com>
2023-04-22 16:59:12 +0800

    git init

---
+++ src/main/java/com/BruceHuntJobApplication.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heyyon
+ * @date 2023-04-22 16:52
+ */
+@SpringBootApplication//去掉数据源
+@MapperScan("com.**.mapper")
+public class BruceHuntJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BruceHuntJobApplication.class,args);
+    }
+
+}
