9dc5309450fd291fddba2f74996d66eab43b9056
heyyon <emailformhy@163.com>
2023-04-25 16:44:35 +0800

    线程、mq

---
+++ src/main/java/com/bruce/designpattern/IUserDao.java
@@ -0,0 +1,6 @@
+package com.bruce.designpattern;
+
+public interface IUserDao {
+    public void save();
+    public void insert();
+}
---
+++ src/main/java/com/bruce/designpattern/UserDao.java
@@ -0,0 +1,16 @@
+package com.bruce.designpattern;
+
+public class UserDao implements IUserDao{
+
+    @Override
+    public void save() {
+        System.out.println("保存数据");
+    }
+
+    @Override
+    public void insert() {
+        System.out.println("插入数据");
+    }
+
+
+}
