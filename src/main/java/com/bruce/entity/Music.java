7bcdf3ef183a374f3f3bddfeb34ab407ca671697
haiyang.meng <emailformhy@163.com>
2024-06-29 18:37:34 +0800

    study

--- src/main/java/com/bruce/entity/Music.java
+++ src/main/java/com/bruce/entity/Music.java
@@ -8,14 +8,36 @@ import java.util.List;
  * @date 2024-06-18 22:24
  */
 public class Music {
+//    static String initStr = "";
+    
     public static void main(String[] args) {
 
-        Integer integer = Integer.valueOf(0);
+//        Integer integer = Integer.valueOf(0);
         List<Instrument> list = new ArrayList<>();
         list.add(new Piano());
         list.add(new Violin());
         for (Instrument instrument : list) {
             instrument.play();
         }
+//        String str = "world";
+//        setStr(str);
+//        System.out.println(str);
+//        String initStr = "";
+//        initStr = "hello world";
+//        initStr = initStr + "1";
+//        setStr(initStr);
+//        printStr(initStr);
     }
+    
+    public static void setStr(String updateStr) {
+        updateStr = updateStr + " hello";
+        System.out.println(updateStr);
+        System.out.println(0);
+    }
+
+    public static void printStr(String updateStr) {
+        System.out.println(updateStr);
+    }
+    
+    
 }
