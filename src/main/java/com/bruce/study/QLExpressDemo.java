e4957d2f0ca20120ead518c6e9329f9e0d1d76bf
haiyang.meng <haiyang.meng@fmaliyun.com>
2025-01-10 17:19:24 +0800

    ql test

---
+++ src/main/java/com/bruce/study/QLExpressDemo.java
@@ -0,0 +1,15 @@
+package com.bruce.study;
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+
+public class QLExpressDemo {
+    public static void main(String[] args) throws Exception {
+        ExpressRunner expressRunner = new ExpressRunner();
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        context.put("a", 10);
+        context.put("b", 20);
+        String express = "a+b*10";
+        Object execute = expressRunner.execute(express, context, null, false, false);
+        System.out.println("执行结果：" + execute);
+    }
+}
--- src/main/java/com/example/demo/aspect/LoggingAspect.java
+++ src/main/java/com/example/demo/aspect/LoggingAspect.java
@@ -1,7 +1,9 @@
 package com.example.demo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,15 +11,25 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(* com.example.demo.service.UserService.addUser(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        System.out.println("Before method:" + joinPoint.getSignature().getName());
-        System.out.println("Arguments:" + joinPoint.getArgs()[0]);
-    }
+//    @Before("execution(* com.example.demo.service.UserService.addUser(..))")
+//    public void logBefore(JoinPoint joinPoint) {
+//        System.out.println("Before method:" + joinPoint.getSignature().getName());
+//        System.out.println("Arguments:" + joinPoint.getArgs()[0]);
+//    }
+//
+//    @After("execution(* com.example.demo.service.UserService.addUser(..))")
+//    public void logAfter(JoinPoint joinPoint) {
+//        System.out.println("After method:" + joinPoint.getSignature().getName());
+//    }
 
-    @After("execution(* com.example.demo.service.UserService.addUser(..))")
-    public void logAfter(JoinPoint joinPoint) {
-        System.out.println("After method:" + joinPoint.getSignature().getName());
+    @Around("execution(* com.example.demo.service.UserService.addUser(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Before method:" + joinPoint.getSignature().getName());
+        Object result = joinPoint.proceed();
+
+        System.out.println("Before after:" + joinPoint.getSignature().getName());
+
+        return result;
     }
 
 }
