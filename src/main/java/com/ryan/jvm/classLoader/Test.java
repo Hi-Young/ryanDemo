d10b4e67ebd9a176ce9a640837163eba76f2157d
haiyang.meng <emailformhy@163.com>
2025-02-04 09:19:46 +0800

    study

--- src/main/java/com/ryan/jvm/classLoader/Test.java
+++ src/main/java/com/ryan/jvm/classLoader/Test.java
@@ -1,6 +1,17 @@
 package com.ryan.jvm.classLoader;
+
 public class Test {
-public void say(){
-System.out.println("Hello HeroClassLoader");
-}
+    public void say() {
+        System.out.println("Hello HeroClassLoader");
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(1000000000);
+        } catch (InterruptedException e) {
+            
+            
+        }
+
+    }
 }
