62a7d97c3ad4024cde36376efc9eceb22df8baff
haiyang.meng <emailformhy@163.com>
2025-02-04 11:23:28 +0800

    study

---
+++ src/main/java/com/ryan/jvm/Test/TestJvmOutOfMemory.java
@@ -0,0 +1,24 @@
+package com.ryan.jvm.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author heyyon
+ * @date 2025-02-04 9:20
+ */
+public class TestJvmOutOfMemory {
+
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            String str = "";
+            for (int j = 0; j < 1000; j++) {
+                str += UUID.randomUUID().toString();
+            }
+            list.add(str);
+        }
+        System.out.println("ok");
+    }
+}
