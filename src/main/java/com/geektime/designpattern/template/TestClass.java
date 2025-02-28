2c450e10bf87a5ea6e9a56e996dd5906dd03acc4
haiyang.meng <haiyang.meng@fmaliyun.com>
2025-02-28 15:09:53 +0800

    study

--- src/main/java/com/geektime/designpattern/template/TestClass.java
+++ src/main/java/com/geektime/designpattern/template/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         Cricket cricket = new Cricket();
-        cricket.test();
+//        cricket.test();
         System.out.println("Hello World");
     }
 }
