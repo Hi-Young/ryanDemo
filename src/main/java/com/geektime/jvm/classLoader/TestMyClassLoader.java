38775afe3938686c89d8fbc97ed2e71cbb257486
heyyon <emailformhy@163.com>
2025-03-18 15:18:45 +0800

    classloader

--- src/main/java/com/geektime/jvm/classLoader/TestMyClassLoader.java
+++ src/main/java/com/geektime/jvm/classLoader/TestMyClassLoader.java
@@ -5,10 +5,10 @@ import java.lang.reflect.Method;
 public class TestMyClassLoader {
     public static void main(String[] args) throws Exception {
         //自定义类加载器的加载路径
-        HeroClassLoader hClassLoader = new HeroClassLoader("D:\\lib");
+        HeroClassLoader hClassLoader = new HeroClassLoader("D:\\PersonalJavaProject\\lib");
         //包名+类名
         // 当前module中虽然有个Test类，但是路径是com.geektime.jvm.classLoader.Test
-        Class c = hClassLoader.loadClass("com.ryan.jvm.classLoader.Test");
+        Class c = hClassLoader.loadClass("com.ryan.jvm.classLoader.Test1");
         if (c != null) {
             Object obj = c.newInstance();
             Method method = c.getMethod("say", null);
