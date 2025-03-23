a1bd51c06cabaa3ab407a716a8ecbe37d8e790ce
heyyon <emailformhy@163.com>
2025-03-23 17:47:56 +0800

    classloader

---
+++ src/main/java/com/ryan/study/WaitNotifyTest.java
@@ -0,0 +1,52 @@
+package com.ryan.study;
+
+public class WaitNotifyTest {
+    static Object o = new Object();
+    static boolean flag = false;
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                synchronized (o) {
+                    System.out.println("start1:" + i);
+
+                    while (flag) {
+
+                        try {
+                            o.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println("生产者生产一个内容");
+                    flag = true;
+                    o.notify();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+
+            for (int i = 0; i < 10; i++) {
+                synchronized (o) {
+                    while (!flag) {
+                        System.out.println("start2:" + i);
+
+
+                        try {
+                            o.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println("消费者消费一个内容");
+                    flag = false;
+                    o.notify();
+
+                }
+            }
+        }).start();
+
+    }
+}
